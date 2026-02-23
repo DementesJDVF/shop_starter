e81f29ed065ce67591632fb9bf0f3fadd38bfb5a
Julian David Vasquez Fernandez <94762757+DementesJDVF@users.noreply.github.com>
2026-02-23 15:11:22 -0500

    Change default user role from 'CUSTOMER' to 'CLIENTE'

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -23,7 +23,7 @@ class RegisterSerializer(serializers.ModelSerializer):
             username=validated_data['username'],
             email=validated_data['email'],
             password=validated_data['password'],
-            role=validated_data.get('role', 'CUSTOMER')
+            role=validated_data.get('role', 'CLIENTE')
         )
         return user
 
