e0bce2c01d9531fe5b312d559a5ec067a95f4ace
Dementes <julian1dave1@hotmail.com>
2026-02-25 17:52:19 -0500

    test(core): mejora cobertura de SoftDelete restore, hard_delete, deleted_only, alive

--- apps/geo/migrations/0001_initial.py
+++ apps/geo/migrations/0001_initial.py
@@ -12,14 +12,12 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='AuditLog',
+            name='Location',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('action', models.CharField(max_length=255)),
-                ('model_name', models.CharField(max_length=100)),
-                ('object_id', models.CharField(max_length=100)),
+                ('latitude', models.FloatField()),
+                ('longitude', models.FloatField()),
                 ('timestamp', models.DateTimeField(auto_now_add=True)),
-                ('ip_address', models.GenericIPAddressField(blank=True, null=True)),
             ],
         ),
     ]
