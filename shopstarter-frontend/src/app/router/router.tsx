042fbf5b54f5f5b4dae44c7a876336d3841a4e1b
Dementes <julian1dave1@hotmail.com>
2026-02-26 22:34:31 -0500

    refactor: dividir la aplicación frontend (React + Vite) y backend (Django)
    - Se desacopla el frontend del backend
    - Se configura nuevo proyecto React con Vite
    - Se mantiene Django como API backend

--- shopstarter-frontend/src/app/router/router.tsx
+++
@@ -1,9 +0,0 @@
-import { createBrowserRouter } from "react-router-dom";
-import LoginPage from "../../pages/LoginPage";
-
-export const router = createBrowserRouter([
-    {
-    path: "/",
-    element: <LoginPage />,
-    },
-]);
