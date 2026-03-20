f1ad137501ad89977c0713982bfc546106cd577c
Aldair ZX <aldairzx6@gmail.com>
2026-03-20 15:35:18 -0500

    correcion del S6-HU02 Cálculo promedio reputación

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import User
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("id", "email", "role", "is_active")
+    list_filter = ("role", "is_active")
+    search_fields = ("email",)
