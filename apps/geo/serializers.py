82a76d95909c71b406ebb5cdcf4ef8c38ea36957
Jhonatan Zemanate Joaqui <zemanatejhonatan857@gmail.com>
2026-03-25 14:57:16 -0500

    nuevo archivo serializer en geo

---
+++ apps/geo/serializers.py
@@ -0,0 +1,6 @@
+from rest_framework import serializers
+from apps.geo.models import Location 
+class LocationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Location
+        fields = ['id', 'latitude', 'longitude', ]
