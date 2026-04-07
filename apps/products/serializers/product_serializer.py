636e2a1bc1f12ac8189db3cdd7c5e0f933a07500
Julian David Vasquez Fernandez <94762757+DementesJDVF@users.noreply.github.com>
2026-04-06 22:04:56 -0500

    Add default value for stock in ProductCreateSerializer
    Set default value for stock field to 0.

--- apps/products/serializers/product_serializer.py
+++ apps/products/serializers/product_serializer.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 
-from apps.products.models import Product
-from apps.products.models import Category
+from apps.products.models import Category, Product
 
 
 class ProductCreateSerializer(serializers.Serializer):
@@ -10,7 +9,7 @@ class ProductCreateSerializer(serializers.Serializer):
     category = serializers.PrimaryKeyRelatedField(queryset=Category.objects.filter(is_deleted=False, is_active=True))
     description = serializers.CharField()
     price = serializers.DecimalField(max_digits=10, decimal_places=2)
-    stock = serializers.IntegerField(required=False, min_value=0)
+    stock = serializers.IntegerField(required=False, min_value=0, default=0)
 
     def validate_price(self, value):
         if value <= 0:
