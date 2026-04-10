281a14616b0b04a2d3f31dab2bf448d96493fdc4
Jhonatan Zemanate Joaqui <zemanatejhonatan857@gmail.com>
2026-04-10 13:23:27 -0500

    subiendo el HU2 de geo

--- apps/geo/urls.py
+++ apps/geo/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import LocationViewSet
+from .views import nearby_vendors
 
 router = DefaultRouter()
 router.register(r"", LocationViewSet, basename="location")
 urlpatterns = [
-    path("", include(router.urls))
+    path("", include(router.urls)),
+    path("nearby/", nearby_vendors),
 ]
