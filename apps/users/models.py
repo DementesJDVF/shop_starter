57d7b9fcd5b678e70d36e7a1146ffe2e48e5a106
Daniel <daniel20012403@gmail.com>
2026-04-15 15:28:56 -0500

    PR: feature/fix-command_createsuperuser

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,10 +1,33 @@
 import uuid
-
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 from .constants import UserRoles
 
+class UserManager(BaseUserManager):
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError("El email es obligatorio")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, password, **extra_fields)
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        # AQUÍ ESTÁ LA MAGIA: Forzamos el rol de ADMIN de tus constantes
+        extra_fields.setdefault("role", UserRoles.ADMIN)
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+        return self._create_user(email, password, **extra_fields)
 
 class User(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
@@ -30,6 +53,8 @@ class User(AbstractUser):
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    
+    objects = UserManager()
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["username"]
