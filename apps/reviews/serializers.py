6ccbd78c746f9c078f0e5abeeb3a304ed567d5f5
Daniel <daniel20012403@gmail.com>
2026-04-15 16:29:39 -0500

    feature/Reviewfix

--- apps/reviews/serializers.py
+++ apps/reviews/serializers.py
@@ -1,11 +1,10 @@
 """Serializers for product endpoints."""
 from rest_framework import serializers
-from apps.orders.models import Order
 from apps.users.models import User
 from apps.reviews.models import Review
 
 class ReviewSerializer(serializers.ModelSerializer):
-    order = serializers.PrimaryKeyRelatedField(queryset=Order.objects.all())
+    vendor = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
     user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
     class Meta:
         model = Review
