5b6059045b680f6d6e9ba142ff9fe1cb99f61abc
dongato <neythanayala670@gmail.com>
2026-04-17 15:56:02 -0500

    feat: crear administrador inicial mediante migración de datos

---
+++ apps/users/migrations/0007_setup_initial_admin.py
@@ -0,0 +1,34 @@
+from django.db import migrations
+from django.contrib.auth import get_user_model
+
+def create_initial_admin(apps, schema_editor):
+    User = get_user_model()
+    email = "neythanayala670@gmail.com"
+    username = "neythan_admin"
+    password = "AdminShop2026*"
+
+    if not User.objects.filter(email=email).exists():
+        User.objects.create_superuser(
+            email=email,
+            username=username,
+            password=password,
+            full_name="Neythan Ayala Admin"
+        )
+        print(f"User {email} created successfully.")
+    else:
+        print(f"User {email} already exists.")
+
+def remove_initial_admin(apps, schema_editor):
+    User = get_user_model()
+    email = "neythanayala670@gmail.com"
+    User.objects.filter(email=email).delete()
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('users', '0006_alter_user_managers'),
+    ]
+
+    operations = [
+        migrations.RunPython(create_initial_admin, reverse_code=remove_initial_admin),
+    ]
