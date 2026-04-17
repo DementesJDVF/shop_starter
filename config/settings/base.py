c2a5ffe607cd0014cd92cfb36df8593bcf6060e2
dongato <neythanayala670@gmail.com>
2026-04-17 15:10:57 -0500

    feat: implementar configuraciones base y de producción en Django con manejo basado en variables de entorno

--- config/settings/base.py
+++ config/settings/base.py
@@ -156,6 +156,8 @@ CORS_ALLOWED_ORIGINS = env.list(
 CSRF_TRUSTED_ORIGINS = env.list(
     "CSRF_TRUSTED_ORIGINS", 
     default=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
         "https://shopstarter.vercel.app", 
         "https://shopstarter.online",
         "http://shopstarter.online",
