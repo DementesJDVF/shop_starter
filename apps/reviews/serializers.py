1e751733cb5a8b2a2247b4f0bfd6839354478b84
Daniel <daniel20012403@gmail.com>
2026-05-15 18:58:39 -0500

    Reviews actualized

--- apps/reviews/serializers.py
+++ apps/reviews/serializers.py
@@ -5,12 +5,13 @@ from apps.reviews.models import Review
 
 class ReviewSerializer(serializers.ModelSerializer):
     client = serializers.ReadOnlyField(source='user.username')
+    client_id = serializers.ReadOnlyField(source='user.id')
     rating = serializers.DecimalField(source='rate', max_digits=2, decimal_places=1)
     review_text = serializers.CharField(source='content', allow_blank=True)
     
     class Meta:
         model = Review
-        fields = ["id", "client", "rating", "review_text", "created_at"]
+        fields = ["id", "client", "rating", "client_id", "review_text", "created_at"]
         read_only_fields = ["user", "created_at"]
 
     def validate(self, data):
